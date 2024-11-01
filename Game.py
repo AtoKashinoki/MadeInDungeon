141fddfc33c329be2fc5551be6755b94ab7cb19d
AtoKashinoki <atokashinoki@gmail.com>
2024-11-01 17:44:08 +0900

    Add game loop

--- Game.py
+++ Game.py
@@ -1,3 +1,6 @@
+from Object import Player
+
+
 def create_map():
     dungeon_map = [
         [
@@ -6,19 +9,31 @@ def create_map():
         ]
         for y in range(20)
     ]
+    dungeon_map[5][5] = 2
 
     return dungeon_map
 
 
-def hierarchy_process():
+def hierarchy_process(player: Player):
     # ダンジョンを生成して変数に保管
     d_map = create_map()
+    game_loop(d_map, player)
 
     return d_map
 
 
-def game_loop(d_map):
+def game_loop(d_map, player: Player):
+    done = False
+    while not done:
+        [print(_line) for _line in d_map]
+        print(player.position)
+        player.move_process(input("input move direction: "))
+        if d_map[player.position[0]][player.position[1]] == 2:
+            break
+
+        """ Enemies process """
+
+        if player.hp <= 0:
+            break
+
     return
-
-
-print(create_map())
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from CodingTools.Inheritance import DataClass
+from Game import hierarchy_process
+from Object import Player
+from CodingTools.Types import Position
 
-class Test(DataClass):
+if __name__ == '__main__':
+    player: Player = Player(Position(2), 0, 0)
+    hierarchy_process(player)
     ...
-
-print(Test())
