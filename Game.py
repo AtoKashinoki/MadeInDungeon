e2f2cf8bc3bb61b1d1e30bf187e16919b064957c
manato <c0b22062c9@edu.teu.ac.jp>
2024-11-01 15:42:27 +0900

    Gane.pyのmove_range、atk_rangeのそれぞれに値を入れました。

--- Game.py
+++ Game.py
@@ -3,14 +3,39 @@ from CodingTools.Inheritance import DataClass
 
 class Setting():
     class Player(DataClass):
-        hp: int
-        atk: int
-        move_range: dict[str, tuple[int, int]]
-        atk_range: dict[str, tuple[int, int]]
+        hp: int = 3
+        atk: int = 1
+        move_range: dict[str, tuple[int, int]] = {
+            "N": (0, -1),
+            "NE": (1, -1),
+            "E": (1, 0),
+            "SE": (1, 1),
+            "S": (0, 1),
+            "SW": (-1, 1),
+            "W": (-1, 0),
+            "NS": (-1, -1)
+
+        }
+        atk_range: dict[str, tuple[int, int]] = {
+            "N": ((-1, -1), (0, -1), (1, -1)),
+            "E": ((1, -1), (1, 0), (1, 1)),
+            "S": ((-1, 1), (0, 1), (1, 1)),
+            "W": ((-1, -1), (-1, 0), (-1, 1)),
+        }
 
     class Enemy(DataClass):
-        hp: int
-        atk: int
-        move_range: dict[str, tuple[int, int]]
-        atk_range: dict[str, tuple[int, int]]
+        hp: int = 2
+        atk: int = 1
+        move_range: dict[str, tuple[int, int]] = {
+            "N": (0, -1),
+            "E": (1, 0),
+            "S": (0, 1),
+            "W": (-1, 0),
+        }
+        atk_range: dict[str, tuple[int, int]] = {
+            "N": (0, -1),
+            "E": (1, 0),
+            "S": (0, 1),
+            "W": (-1, 0),
+        }
         options: dict[str, int | float]
