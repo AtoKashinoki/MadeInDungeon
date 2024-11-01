90e3e5a13a1ba9520e696f88d1c9cb8221761b14
manato <c0b22062c9@edu.teu.ac.jp>
2024-11-01 15:09:45 +0900

    Settingの内容を変更しました。

--- Game.py
+++ Game.py
@@ -5,12 +5,12 @@ class Setting():
     class Player(DataClass):
         hp: int
         atk: int
-        move_range: tuple[tuple[int, int]]
-        atk_range: tuple[tuple[int, int]]
+        move_range: dict[str, tuple[int, int]]
+        atk_range: dict[str, tuple[int, int]]
 
     class Enemy(DataClass):
         hp: int
         atk: int
-        move_range: tuple[tuple[int, int]]
-        atk_range: tuple[tuple[int, int]]
+        move_range: dict[str, tuple[int, int]]
+        atk_range: dict[str, tuple[int, int]]
         options: dict[str, int | float]
