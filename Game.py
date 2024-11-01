072b04aba13311080832b87a33af518efc33d1d0
manato <c0b22062c9@edu.teu.ac.jp>
2024-11-01 17:13:08 +0900

    階層処理を作成しました。

---
+++ Game.py
@@ -0,0 +1,24 @@
+def create_map():
+    dungeon_map = [
+        [
+            0 if 0 < x < 19 and 0 < y < 19 else 1
+            for x in range(20)
+        ]
+        for y in range(20)
+    ]
+
+    return dungeon_map
+
+
+def hierarchy_process():
+    # ダンジョンを生成して変数に保管
+    d_map = create_map()
+
+    return d_map
+
+
+def game_loop(d_map):
+    return
+
+
+print(create_map())
