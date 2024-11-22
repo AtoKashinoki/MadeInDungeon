78bdff2e4b7da41ae9ab0ae84e2c9669886edc5e
izumi <c0b22012ca@edu.teu.ac.jp>
2024-11-22 17:58:55 +0900

    add comment

--- src/MyAI.py
+++ src/MyAI.py
@@ -3,11 +3,10 @@
 
 This file contain game AIs.
 """
-from src.Object import Enemy, PlayerStatus
+
 
 """ imports """
-
-
+from src.Object import Enemy, PlayerStatus
 from src.AISkeleton import AISkeleton
 
 
@@ -29,12 +28,13 @@ class MainAI(AISkeleton):
 
     def __move_process__(
             self,
-            _visibility_map: tuple[tuple[int]],
-            _player_status: PlayerStatus,
-            _enemies: tuple[Enemy, ...],
+            _visibility_map: tuple[tuple[int]],#プレイヤーの見えてるMAP
+            _player_status: PlayerStatus,#
+            _enemies: tuple[Enemy, ...],#MAP上で見えている敵のクラスのタプル=敵の位置わかるね＝
     ) -> str:
-        input_key = "W"
+        input_key = "W"#ここに行動したいやつを入力する.
         return input_key
+    #上記のやつつくれ
 
     ...
 
